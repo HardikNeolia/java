ec18be140f04602142bb971041660593dffb413e
HardikNeolia <hardikneolia@gmail.com>
2024-06-11 13:17:07 +0000

    datatypes

--- 01_basics/01_dataTypes.js
+++ 01_basics/01_dataTypes.js
@@ -2,5 +2,28 @@
 
 // alert( 3 + 3 ) //we are using node js not browser
 
-console.log( 3 + 3 ) 
+console.log( 3 
+    + 
+    3 )  // code readability should be  high
+
  console.log("hardik")
+
+
+ let name = "hardik"
+ let age = 18
+ let isloggedIn = false
+let state;
+
+ // number => 2 to power 53
+ // bigint
+ // string => ""
+ // boolean => true/false
+ // null => standalone value
+ // undefined =>
+ // symbol => unique
+ // object
+
+
+
+ console.log(typeof undefined ); // undefined
+ console.log(typeof null );  // object
