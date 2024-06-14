4624acdcd8bd244ba178b049e76f2e432af900d0
HardikNeolia <hardikneolia@gmail.com>
2024-06-14 12:52:17 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,54 @@
+// # PRIMITIVE
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id===anotherId);
+
+// const bigNumber =345678901233345555765655688n
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman","naagraj", "doga"]
+let myObj ={
+    name: "hardik",
+    age: 18,
+}
+
+
+
+const myfunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof anotherId);
+
+
+
+/* 1)  PRIMITIVE DATATYPES
+       NUMBER => number
+       STRING => string
+       BOOLEAN => boolean
+       NULL => object
+       UNDEFINED => undefined
+       SYMBOL => symbol
+       BIGINT =>  bigint
+
+
+   2)  NON-PRIMITIVE DATATYPES
+       ARRAYS => object
+       FUNCTIONS => function
+       OBJECT => object
+
+       */
