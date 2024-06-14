ae60cd179eaab700c3c07ba49f058333dd0b2428
HardikNeolia <hardikneolia@gmail.com>
2024-06-14 13:48:49 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,16 @@
+const name = "hardik"
+const repoCount = 10
+
+// console.log(name + repoCount + " value");
+// console.log(`hello my name is ${name} and my repo Count is ${repoCount} `);
+
+
+const gameName = new String('hardikn')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt('2'));
+console.log(gameName.indexOf('r'));
