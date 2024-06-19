93c91195245b7d24631a0282781662036e121c28
HardikNeolia <hardikneolia@gmail.com>
2024-06-19 13:57:54 +0000

    string

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -14,3 +14,22 @@ const gameName = new String('hardikn')
 // console.log(gameName.toUpperCase());
 console.log(gameName.charAt('2'));
 console.log(gameName.indexOf('r'));
+
+const newString = gameName.substring(0, 4) 
+console.log(newString);
+
+const anotherString = gameName.slice(-6, 2)
+console.log(anotherString);
+
+
+const newStringOne = "   hardik    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hardik.com/hardik%20neolia"
+
+console.log(url.replace('%20', '-' ));
+
+console.log(url.includes('kartik'));
+
+console.log(gameName.split('-'));
